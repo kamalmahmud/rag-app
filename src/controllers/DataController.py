9d9b2dd5fb03efb16cd9c966473d4967017344ff
U220709704 <kamalmialqedra@posta.mu.edu.tr>
2026-06-24 23:14:58 +0300

    update

---
+++ src/controllers/DataController.py
@@ -0,0 +1,18 @@
+from .BaseController import BaseController
+from fastapi import UploadFile
+from models import ResponseSignal
+
+class DataController(BaseController):
+    def __init__(self):
+        super().__init__()
+        self.size_scale = 1048576
+
+    def validate_uploaded_file(self,file:UploadFile):
+        if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
+            return False,ResponseSignal.FILE_TYPE_NOT_SUPPORTED
+        
+        if file.size > (self.app_settings.FILE_MAX_SIZE * self.size_scale):
+            return False,ResponseSignal.FILE_SIZE_EXCEEDED
+        
+        return True,ResponseSignal.FILE_UPLOAD_SUCCESS
+    
