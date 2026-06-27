eeb0cb8f660387c07be22571a2235cb60eaf7d9a
U220709704 <kamalmialqedra@posta.mu.edu.tr>
2026-06-27 15:14:25 +0300

    feat: add file upload functionality

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,6 +1,14 @@
-from fastapi import APIRouter,Depends,UploadFile
+from fastapi import APIRouter,Depends,UploadFile,status
+from fastapi.responses import JSONResponse
 from helpers.config import get_settings,Settings
-from controllers import DataController
+from controllers import DataController,ProjectController
+import os
+import aiofiles
+from models import ResponseSignal
+import logging
+
+logger = logging.getLogger('uvicorn.error')
+
 data_router = APIRouter(
     prefix="/api/v1/data",
     tags=["api_v1","data"]
@@ -8,5 +16,42 @@ data_router = APIRouter(
 
 @data_router.post("/upload/{project_id}")
 async def upload_data(project_id: str, file:UploadFile,app_settings: Settings=Depends(get_settings)):
-    is_valid,response = DataController().validate_uploaded_file(file=file) 
-    return response
+    data_controller = DataController()
+
+    is_valid,response = data_controller.validate_uploaded_file(file=file) 
+    
+    if not is_valid:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal":response,
+            }
+        )
+    
+    project_dir_path = ProjectController().get_project_path(project_id=project_id)
+
+    file_path = data_controller.generate_unique_filename(
+        orig_file_name=file.filename,
+        project_id=project_id
+        )
+     
+    try:
+        async with aiofiles.open(file_path,"wb") as f:
+            while chunk := await file.read(app_settings.FILE_DEFULT_CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception as e:
+
+        logger.error(f"Error while uplodong file: {e}")
+
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal":ResponseSignal.FILE_UPLOAD_FAILED.value
+            }
+        )
+
+    return JSONResponse(
+            content={
+                "signal":response,
+            }
+        )
