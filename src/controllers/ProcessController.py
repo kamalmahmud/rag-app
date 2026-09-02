dac1ab2124c356a660c158ddea825c3b4fbbd71e
U220709704 <kamalmialqedra@posta.mu.edu.tr>
2026-09-02 17:06:49 +0300

    improving the data pipeline

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -2,37 +2,43 @@ import os
 
 from .BaseController import BaseController
 from .ProjectController import ProjectController
-from langchain_community.document_loaders import TextLoader,PyMuPDFLoader
+from langchain_community.document_loaders import TextLoader, PyMuPDFLoader
 from models import ProcessingEnum
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+
 class ProcessController(BaseController):
-    def __init__(self,project_id:str):
+    def __init__(self, project_id: str):
         super().__init__()
         self.project_id = project_id
         self.project_path = ProjectController().get_project_path(project_id)
 
-    def get_file_extension(self,file_id:str):
+    def get_file_extension(self, file_id: str):
         return os.path.splitext(file_id)[-1]
 
-    def get_file_loader(self,file_id:str):
+    def get_file_loader(self, file_id: str):
 
         file_ext = self.get_file_extension(file_id)
-        file_path = os.path.join(self.project_path,file_id)
-        if file_ext == ProcessingEnum.TXT.value:
+        file_path = os.path.join(self.project_path, file_id)
 
-            return TextLoader(file_path,encoding="utf-8")
+        if not os.path.exists(file_path):
+            return None
+
+        if file_ext == ProcessingEnum.TXT.value:
+            return TextLoader(file_path, encoding="utf-8")
 
         if file_ext == ProcessingEnum.PDF.value:
             return PyMuPDFLoader(file_path)
         return None
 
-    def get_file_content(self,file_id:str):
+    def get_file_content(self, file_id: str):
         loader = self.get_file_loader(file_id)
-        return loader.load()
+        if loader:
+            return loader.load()
+        return None
 
-    def process_file_content(self,file_content:list,file_id:str,
-                             chunk_size:int = 100, overlap_size:int = 20):
+    def process_file_content(self, file_content: list, file_id: str,
+                             chunk_size: int = 100, overlap_size: int = 20):
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=chunk_size,
             chunk_overlap=overlap_size,
@@ -54,4 +60,3 @@ class ProcessController(BaseController):
         )
 
         return chunks
-
--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -1,6 +1,8 @@
 from .db_schemes import Project
 from .BaseDataModel import BaseDataModel
 from .enums import DataBaseEnum
+
+
 class ProjectModel(BaseDataModel):
     def __init__(self, db_client: object):
         super().__init__(db_client)
@@ -24,7 +26,7 @@ class ProjectModel(BaseDataModel):
 
     async def create_project(self, project:Project):
         result = await self.collection.insert_one(project.dict(by_alias=True, exclude_unset=True))
-        project._id = result.inserted_id
+        project.id = result.inserted_id
         return project
 
     async def get_project_or_create_one(self, project_id:str):
